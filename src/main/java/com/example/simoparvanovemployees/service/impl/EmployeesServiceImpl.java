72433a41858253ee0a33fa12a830346e4ff42745
Simo-Parvanov <simo86java@gmail.com>
2021-08-24 04:47:44 +0300

    Refactoring

--- src/main/java/com/example/simoparvanovemployees/service/impl/EmployeesServiceImpl.java
+++ src/main/java/com/example/simoparvanovemployees/service/impl/EmployeesServiceImpl.java
@@ -18,6 +18,11 @@ import java.util.*;
 
 @Service
 public class EmployeesServiceImpl implements EmployeesService {
+    public static String[] variations = new String[2];
+    public static Set<String> setAllElements = new HashSet<>();
+    public static Set<String> setMomentElements = new HashSet<>();
+    public static List<String> elements = new ArrayList<>();
+    public static List<String> uniqueCouples = new ArrayList<>();
     private static final String[] formats = {
             "yyyy-MM-dd'T'HH:mm:ss'Z'", "yyyy-MM-dd'T'HH:mm:ssZ",
             "yyyy-MM-dd'T'HH:mm:ss", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",
@@ -46,6 +51,7 @@ public class EmployeesServiceImpl implements EmployeesService {
                     String projectID = date[1];
                     String dateFrom = date[2];
                     String dateTo = date[3];
+                    setAllElements.add(empID);
 
                     Employees employees = new Employees(
                             empID,
@@ -61,108 +67,107 @@ public class EmployeesServiceImpl implements EmployeesService {
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
-        return mapBestEmployees(twoEmployeesWithTheLongestTime(employeesList));
+        elements.addAll(setAllElements);
+        return getBestEmployees(employeesList);
     }
 
-    private List<BestEmployees> mapBestEmployees(List<String> result) {
+    private List<BestEmployees> getBestEmployees(List<Employees> employeesList) {
+        HashMap<String, HashMap<String, Long>> mapBestEmployees = getMapPyProject(employeesList);
         List<BestEmployees> bestEmployees = new ArrayList<>();
+        for (Map.Entry<String, HashMap<String, Long>> entry : mapBestEmployees.entrySet()) {
+            String[] split = entry.getKey().split("@");
+            String emp1 = split[0];
+            String emp2 = split[1];
+            for (Map.Entry<String, Long> employee : entry.getValue().entrySet()) {
 
-            BestEmployees emp = new BestEmployees();
-            emp.setProjectId(result.get(0));
-            emp.setEmpIdOne(result.get(1));
-            emp.setEmpIdTwo(result.get(2));
-            emp.setAllDays(Long.parseLong(result.get(3)) + Long.parseLong(result.get(4)));
-            bestEmployees.add(emp);
+                BestEmployees emp = new BestEmployees();
+                emp.setProjectId(employee.getKey());
+                emp.setEmpIdOne(emp1);
+                emp.setEmpIdTwo(emp2);
+                emp.setAllDays(employee.getValue());
+                bestEmployees.add(emp);
+            }
+        }
         return bestEmployees;
     }
 
-    private static List<String> twoEmployeesWithTheLongestTime(List<Employees> employeesList) {
+    private static HashMap<String, HashMap<String, Long>> getMapPyProject(List<Employees> employeesList) {
         HashMap<String, HashMap<String, Long>> mapProject = new HashMap<>();
+        comb(0, 0);
         for (Employees employees : employeesList) {
             long noOfDaysBetween = ChronoUnit.DAYS.between(employees.getStartDate(), employees.getEndDate());
             mapProject.putIfAbsent(employees.getProjectId(), new HashMap<>());
             mapProject.get(employees.getProjectId()).put(employees.getEmpId(), noOfDaysBetween);
         }
-        return sortEmployeesDays(mapProject);
+        return getMapByUniqueCouplesEmployees(mapProject);
     }
 
-    private static List<String> sortEmployeesDays(HashMap<String, HashMap<String, Long>> mapProject) {
-        String project;
-        String empOneTest = "";
-        String empTwoTest = "";
-        ;
-        long dayEmpOneTest = 0;
-        long dayEmpTwoTest = 0;
+    private static HashMap<String, HashMap<String, Long>> getMapByUniqueCouplesEmployees(HashMap<String, HashMap<String, Long>> mapProject) {
+        HashMap<String, HashMap<String, Long>> mapProjectByEmployees = new HashMap<>();
+        HashMap<String, Long> mapEmployeesForOneProject = new HashMap<>();
 
-        String projectFinal = "";
-        String empOneFinal = "";
-        ;
-        String empTwoFinal = "";
-        ;
-        long dayEmpOneFinal = 0;
-        long dayEmpTwoFinal = 0;
-
-        for (Map.Entry<String, HashMap<String, Long>> mapEntry : mapProject.entrySet()) {
-            if (mapEntry.getValue().size() == 1) continue;
-
-            project = mapEntry.getKey();
-            HashMap<String, Long> mapEmp = mapEntry.getValue();
-            Map<String, Long> hm1 = sortByValue(mapEmp);
-            int count = 0;
-            for (Map.Entry<String, Long> en : hm1.entrySet()) {
-                count++;
-                if (count == 1) {
-                    empOneTest = en.getKey();
-                    dayEmpOneTest = en.getValue();
-                }
-                if (count == 2) {
-                    empTwoTest = en.getKey();
-                    dayEmpTwoTest = en.getValue();
-                    break;
-                }
+        for (Map.Entry<String, HashMap<String, Long>> entry : mapProject.entrySet()) {
+            HashMap<String, Long> mapEmp = entry.getValue();
+            for (Map.Entry<String, Long> entryEmployee : mapEmp.entrySet()) {
+                setMomentElements.add(entryEmployee.getKey());
+                mapEmployeesForOneProject.put(entryEmployee.getKey(), entryEmployee.getValue());
             }
-            if (projectFinal.isEmpty()) {
-                projectFinal = project;
-                empOneFinal = empOneTest;
-                empTwoFinal = empTwoTest;
-                dayEmpOneFinal = dayEmpOneTest;
-                dayEmpTwoFinal = dayEmpTwoTest;
-            } else {
-                if (dayEmpOneTest + dayEmpTwoTest > dayEmpOneFinal + dayEmpTwoFinal) {
-                    projectFinal = project;
-                    empOneFinal = empOneTest;
-                    empTwoFinal = empTwoTest;
-                    dayEmpOneFinal = dayEmpOneTest;
-                    dayEmpTwoFinal = dayEmpTwoTest;
+            extracted(mapProjectByEmployees, mapEmployeesForOneProject, entry);
+            mapEmployeesForOneProject.clear();
+        }
+        return selectTheBestCouple(mapProjectByEmployees);
+    }
+
+    private static void extracted(HashMap<String, HashMap<String, Long>> mapProjectByEmployees,
+                                  HashMap<String, Long> mapEmployeesForOneProject, Map.Entry<String,
+            HashMap<String, Long>> entry) {
+        elements.clear();
+        uniqueCouples.clear();
+        elements.addAll(setMomentElements);
+        comb(0, 0);
+        setMomentElements.clear();
+        for (String uniqueCouple : uniqueCouples) {
+            String[] split = uniqueCouple.split("@");
+            String emp1 = split[0];
+            String emp2 = split[1];
+            long valEmp1 = mapEmployeesForOneProject.get(emp1);
+            long valEmp2 = mapEmployeesForOneProject.get(emp2);
+            if (!mapProjectByEmployees.containsKey(uniqueCouple)) {
+                mapProjectByEmployees.put(uniqueCouple, new HashMap<>());
+                mapProjectByEmployees.get(uniqueCouple).put(entry.getKey(), 0L);
+            }
+            mapProjectByEmployees.get(uniqueCouple).putIfAbsent(entry.getKey(), 0L);
+            mapProjectByEmployees.get(uniqueCouple).put(entry.getKey(),
+                    mapProjectByEmployees.get(uniqueCouple).get(entry.getKey()) + valEmp1 + valEmp2);
+        }
+    }
+
+    private static HashMap<String, HashMap<String, Long>> selectTheBestCouple(HashMap<String, HashMap<String, Long>> mapProject) {
+        HashMap<String, HashMap<String, Long>> finalMap = new HashMap<>();
+        String kay = "";
+        long sum = 0;
+        for (Map.Entry<String, HashMap<String, Long>> entry : mapProject.entrySet()) {
+            if (entry.getValue().size() > 1) {
+                long a = entry.getValue().values().stream().reduce(0L, Long::sum);
+                if (a > sum) {
+                    kay = entry.getKey();
+                    sum = a;
                 }
             }
         }
-        List<String> result = new ArrayList<>();
-        result.add(projectFinal);
-        result.add(empOneFinal);
-        result.add(empTwoFinal);
-        result.add(String.valueOf(dayEmpOneFinal));
-        result.add(String.valueOf(dayEmpTwoFinal));
-
-        return result;
+        finalMap.put(kay, mapProject.get(kay));
+        return finalMap;
     }
 
-    private static HashMap<String, Long> sortByValue(HashMap<String, Long> hm) {
-        List<Map.Entry<String, Long>> list =
-                new LinkedList<Map.Entry<String, Long>>(hm.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<String, Long>>() {
-            public int compare(Map.Entry<String, Long> o1,
-                               Map.Entry<String, Long> o2) {
-                return (o2.getValue()).compareTo(o1.getValue());
+    public static void comb(int index, int start) {
+        if (index >= variations.length) {
+            uniqueCouples.add(variations[0] + "@" + variations[1]);
+        } else {
+            for (int i = start; i < elements.size(); i++) {
+                variations[index] = elements.get(i);
+                comb(index + 1, i + 1);
             }
-        });
-
-        HashMap<String, Long> temp = new LinkedHashMap<String, Long>();
-        for (Map.Entry<String, Long> aa : list) {
-            temp.put(aa.getKey(), aa.getValue());
         }
-        return temp;
     }
 
     private static LocalDate dateFormatter(String dateFrom) {
